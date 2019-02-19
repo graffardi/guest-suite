e3caa9ac5026d2f5bdb8612b090b5932940a44f6
Unknown <victor.graffard@gmail.com>
2019-02-19 16:38:23 +0100

    Removed interface repetition

--- src/components/bar/MetricItem.tsx
+++ src/components/bar/MetricItem.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-interface Props {
-  name: string
-  value: number
-  unit?: string
-}
+import { IMetricsField } from '../../stores/MetricsContext'
 
-const MetricItem = (props: Props) => (
+const MetricItem = <Props extends IMetricsField>(props: Props) => (
   <div className="metric-container">
     <div className="metric-name">{props.name}</div>
     <div className="metric-value">{`${props.value} ${props.unit ? props.unit : ''}`}</div>
